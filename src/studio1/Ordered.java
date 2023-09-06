f30c0e4aac422dd222c6fdae6b71115eaa5950d8
march <march@DESKTOP-E3N1F3L.dhcp.wustl.edu>
2023-09-06 17:06:29 -0500

    Studio 1 complete

--- src/studio1/Ordered.java
+++ src/studio1/Ordered.java
@@ -8,6 +8,7 @@ public class Ordered {
 	 * @param args
 	 */
 	public static void main(String[] args) {
+		
 		Scanner in = new Scanner(System.in);
 		System.out.println("Value for x?");
 		int x = in.nextInt();
@@ -15,6 +16,14 @@ public class Ordered {
 		int y = in.nextInt();
 		System.out.println("Value for z?");
 		int z = in.nextInt();
+		boolean a1 = x > y;
+		boolean a2 = y > z;
+		boolean aa = (a1 && a2);
+		boolean b1 = x < y;
+		boolean b2 = y < z;
+		boolean bb = (b1 && b2);
+		boolean isOrdered = (aa || bb);
+		System.out.println(isOrdered);
 
 	}
 
